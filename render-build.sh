837b85a21d57ba0e35b21290dbda5a93e779bc03
Rashad <albazlir@gmail.com>
2026-02-06 07:14:20 +0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# تثبيت ffmpeg و nodejs
-# Render سيقوم بتشغيل هذا كـ root تلقائياً عند استخدامه بشكل صحيح
-apt-get update && apt-get install -y ffmpeg nodejs
-
-# تثبيت مكتبات بايثون
-pip install -r requirements.txt
